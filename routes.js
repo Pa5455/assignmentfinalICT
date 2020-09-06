3473b8cbcc72f46743cfcd1f48f1607928109376
Glitch (assignment-ict) <none>
2020-09-06 19:12:30 +0000

    ❤️🏡 Checkpoint ./controllers/accounts.js:18261450/69 ./routes.js:18261450/418 ./views/partials/menu.hbs:18261450/60 ./views/partials/addassessmentlist.hbs:18261450/37

--- routes.js
+++ routes.js
@@ -8,12 +8,17 @@ const router = express.Router();
 const dashboard = require("./controllers/dashboard.js");
 const about = require("./controllers/about.js");
 const assessmentlist = require('./controllers/assessmentlist.js');
+const accounts = require('./controllers/accounts.js');
+
+router.get('/', accounts.index);
+router.get('/login', accounts.login);
+router.get('/signup', accounts.signup);
+router.get('/logout', accounts.logout);
+router.post('/register', accounts.register);
+router.post('/authenticate', accounts.authenticate);
 
 
-
-
-
-router.get("/", dashboard.index);
+//router.get("/", dashboard.index);
 router.get("/dashboard", dashboard.index);
 router.get('/dashboard/deleteassessmentlist/:id', dashboard.deleteAssessmentlist);
 router.post('/dashboard/addassessmentlist', dashboard.addAssessmentlist);
